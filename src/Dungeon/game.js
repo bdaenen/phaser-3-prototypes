87a3acb72a483736626ecfe3a69541ff36c68ea3
benno.daenen <benno.daenen@tobania.be>
2018-03-07 13:22:15 +0100

    [Dungeon] Basic scrolling map example.

--- src/Dungeon/game.js
+++ src/Dungeon/game.js
@@ -6,7 +6,7 @@
   var config = {
     type: Phaser.AUTO,
     width: 960,
-    height: 540,
+    height: 544,
     pixelArt: true,
     physics: {
       default: 'arcade',
@@ -24,7 +24,7 @@
       images: [
         { key: 'placeholder', url: './assets/placeholder.png' },
         { key: 'placeholder-borderless', url: './assets/placeholder_borderless.png' },
-        { key: 'maze_64', url: './assets/maze_64.png' }
+        { key: 'dungeon', url: './assets/dungeon.png' }
       ]
     }
   };
@@ -47,40 +47,49 @@
     config.files.images.forEach(function(file){
       this.load.image(file.key, file.url);
     }, this);
-    this.load.tilemapTiledJSON('map', 'assets/dungeon.json');
+    this.load.tilemapTiledJSON('map', 'assets/dungeon1_reworked.json');
   }
 
   function create ()
   {
+    map = this.make.tilemap({ key: 'map' });
+    var tiles = map.addTilesetImage('dungeon');
+    var bgLayer = map.createStaticLayer('bg', tiles, 0, 0);
+    var collisionLayer = map.createStaticLayer('collision', tiles, 0, 0);
+    collisionLayer.visible = false;
     backgroundLayer = this.add.group();
     objectLayer = this.add.group();
     foregroundLayer = this.add.group();
     player = this.physics.add.sprite(100, 450, 'placeholder');
     player.scaleX = 0.75;
     player.scaleY = 0.75;
-    objectLayer.add(player);
-    player.x = 150;
-    player.y = 2550;
+
     console.log(player);
-    // Figure out why this doesn't work.
-//    player.setCollideWorldBounds(true);
+
+    objectLayer.add(player);
+    player.x = 100;
+    player.y = 100;
+
+    // Scaling bug?
+    player.body.offset.x = 0.20 * player.width;
+    player.body.offset.y = 0.20 * player.height;
 
     keys = this.input.keyboard.createCursorKeys();
+    collisionLayer.setCollision(235);
+    var doorKeys = map.createFromObjects('objects', 4, { key: 'placeholder' });
+    var spawn = map.createFromObjects('objects', 2, { key: 'placeholder_borderless' });
 
-    map = this.make.tilemap({ key: 'map' });
-    var tiles = map.addTilesetImage('maze_64');
-    var layer = map.createStaticLayer('Tilelaag 1', tiles, 0, 0);
-
-    map.setCollision(1);
-    this.physics.add.collider(player, layer);
+    this.physics.add.collider(player, collisionLayer);
+    this.cameras.main.roundPixels = true;
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-    this.cameras.main.startFollow(player);
+    //this.cameras.main.startFollow(player);
 
   }
 
   function update ()
   {
     updateMovement();
+    updateCameraPosition(this.cameras.main);
   }
 
   function updateMovement() {
@@ -98,5 +107,27 @@
     }
     player.setVelocityX(xVelocity);
     player.setVelocityY(yVelocity);
+  }
+
+  function updateCameraPosition(cam) {
+    var bounds = {
+      xMin: cam.scrollX,
+      xMax: cam.scrollX + cam.width,
+      yMin: cam.scrollY,
+      yMax: cam.scrollY + cam.height
+    };
+    //cam.x--;
+    if (bounds.xMin > player.x) {
+      cam.scrollX -= cam.width;
+    }
+    if (bounds.xMax < player.x) {
+      cam.scrollX+= cam.width;
+    }
+    if (bounds.yMin > player.y) {
+      cam.scrollY -= cam.height;
+    }
+    if (bounds.yMax < player.y) {
+      cam.scrollY += cam.height;
+    }
   }
 }());
